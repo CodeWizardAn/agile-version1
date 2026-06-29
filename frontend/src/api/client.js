93bbe296e8df2180f4197d038bfc4ea91fdaf8d3
CodeWizardAn <codewizardan@gmail.com>
2026-06-29 20:21:26 +0530

    Add global axios interceptor for 500 and network errors

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,8 +1,21 @@
 import axios from 'axios'
+import { toast } from '../utils/toast'
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000',
   withCredentials: true,
 })
+
+api.interceptors.response.use(
+  res => res,
+  err => {
+    if (!err.response) {
+      toast('Cannot reach the server. Please check your connection.')
+    } else if (err.response.status >= 500) {
+      toast(err.response.data?.detail || 'Something went wrong on the server. Please try again.')
+    }
+    return Promise.reject(err)
+  }
+)
 
 export default api
